b0705dffc475184380bf0a920b24ce4e9014df5c
Corona_chan <romain.lesoin14@gmail.com>
2023-01-26 16:03:29 +0100

    début projet

---
+++ food_exclusion/Connector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace food_exclusion
+{
+    internal class Connector
+    {
+        public Connector() { }
+
+
+
+        public async Task<String[]> Request(string barCode)
+        {
+
+            var client = new HttpClient();
+            var result = await client.GetAsync($"https://world.openfoodfacts.org/api/v2/search?fields={barCode}");
+
+            
+            Console.WriteLine( result.EnsureSuccessStatusCode());
+            String[] content = new[] { await result.Content.ReadAsStringAsync() };
+            
+            return content;
+        }
+    }
+}
---
+++ food_exclusion/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using food_exclusion;
+
+Console.WriteLine("Hello, World!");
+Connector test_co = new Connector();
+
+var answer = await test_co.Request("3250391781836");
+Console.WriteLine(answer);
